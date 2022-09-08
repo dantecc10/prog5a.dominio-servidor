5c230e4de8fba925846bdd406199b96d19176767
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-09-08 14:09:30 -0500

    Botón de inicio de sesión

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dante - Inicio</title>
+    <title>PROG5A - Inicio</title>
 </head>
 
 <body>
@@ -24,7 +24,7 @@
                 </a>
                 <div class="Menú-Desplegable"> <button class="Botón-Desplegar">Menú</button>
                     <div class="Contenido-Desplegable">
-                        <a href="IniciarSesión.php">Iniciar Sesión</a>
+                        <button onclick="document.getElementById('DivInicioSesión').style.display='block'" class="IniciarSesión DecoradoBotón" style="width:auto;" id="BotónAbreInicioSesión">Iniciar sesión</button>
                         <a href="https://politiza.castelancarpinteyro.club/">POLITIZA MX</a>
                         <a href="ProgramasPC.php">Programas avanzados para PC</a>
                         <a href="AcercaDe.php">Acerca de...</a>
@@ -36,6 +36,39 @@
         <div id="Encabezado-Superior"></div>
         <div id="Encabezado-Inferior"></div>
     </header>
+
+    <div id="DivInicioSesión" class="modal">
+        <form class="modal-content animate" action="" method="post">
+            <div class="imgcontainer">
+                <span onclick="document.getElementById('DivInicioSesión').style.display='none'" class="CerrarCuadro" title="Close Modal">×</span>
+                <img src="IMG/img_avatar2.png" alt="Avatar" class="avatar">
+            </div>
+
+            <div class="container">
+                <label for="usuario"><b>Usuario</b></label>
+                <input type="text" placeholder="Usuario (en mayúsculas)*" name="usuario" required="">
+                <br>
+                <label for="contraseña"><b>Contraseña</b></label>
+                <input type="password" placeholder="Contraseña (en minúsculas)*" name="contraseña" required="">
+
+                <input type="submit" name="InicioSesión" value="Iniciar sesión" class="IniciarSesión DecoradoBotón">
+
+                <label>
+                    <input type="checkbox" checked="checked" name="remember">Recordarme
+                </label>
+
+            </div>
+            <?php
+            include "Modelo/ConexiónSeguridad.php";
+            //include "Controlador/controlador_login_tercero.php";
+            ?>
+
+            <div class="container" style="background-color:#f1f1f1">
+                <button type="button" onclick="document.getElementById('DivInicioSesión').style.display='none'" class="BotónCancelar DecoradoBotón">Cancel</button>
+                <span class="psw">¿Olvidaste tu <a href="#">contraseña</a>?</span>
+            </div>
+        </form>
+    </div>
 
     <h2 id="IndicaArtículos">Mi página de inicio</h2>
     <div class="Contenedor-Artículos" id="PartidosPolíticos">
