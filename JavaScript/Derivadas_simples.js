2305f234867466908c15b8b7500919f6ee48b22b
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-09-08 20:35:00 -0500

    formato por Dante

--- JavaScript/Derivadas_simples.js
+++ JavaScript/Derivadas_simples.js
@@ -1,25 +1,25 @@
 function descomponerstring() {
     let arr = mensaje.split(' ');
 
-// El arreglo
-console.log(arr); // ["Soy", "un", "tipo", "feliz", "y", "afortunado"]
+    // El arreglo
+    console.log(arr); // ["Soy", "un", "tipo", "feliz", "y", "afortunado"]
 
 
-// Acceso a cada elemento del arreglo resultante
-console.log(arr[0]); // "Soy"
-console.log(arr[1]); // "un"
-console.log(arr[2]); // "tipo"
-console.log(arr[3]); // "feliz"
-console.log(arr[4]); // "y",
-console.log(arr[5]); // "afortunado"
+    // Acceso a cada elemento del arreglo resultante
+    console.log(arr[0]); // "Soy"
+    console.log(arr[1]); // "un"
+    console.log(arr[2]); // "tipo"
+    console.log(arr[3]); // "feliz"
+    console.log(arr[4]); // "y",
+    console.log(arr[5]); // "afortunado"
 
 }
 
 var nums = "1234567890";
-var abc= "AaBbCcDdEeFfGgHhIiJjKkLlMmNnÑñOoPpQqRrSsTtUuVvWwXxYyZz"
+var abc = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnÑñOoPpQqRrSsTtUuVvWwXxYyZz"
 function esnumero(texto) {
     for (let i = 0; i < texto.length; i++) {
-        if (nums.indexOf(texto.charAt(i),0)!=-1) {
+        if (nums.indexOf(texto.charAt(i), 0) != -1) {
             return 1;
         }
         return 0;
