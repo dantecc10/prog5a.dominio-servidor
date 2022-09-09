afffd0bed57d0d15be388ef056b6bed3683b90ee
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-09-08 22:25:21 -0500

    otro tipo de conexión

--- Scripts PHP/ConexiónUsuarios.php
+++ Scripts PHP/ConexiónUsuarios.php
@@ -1,3 +1,13 @@
 <?php
-$conexiónSeguridad = new mysqli("localhost", "prog5a", "@prog5a!!", "prog5a");
-$conexiónSeguridad->set_charset("utf8");
+
+$servername = "localhost";
+$database = $dbname = "prog5a";
+$username = "prog5a";
+$password = "@prog5a!!";
+// Create connection
+$conexión = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conexión) {
+    die("<p>Error al conectar con la base de datos: " . mysqli_connect_error() . "</p><p>Pruebe a verificar la conexión del servidor...</p>");
+}
+echo "Conexión a la base de datos: exitosa";
