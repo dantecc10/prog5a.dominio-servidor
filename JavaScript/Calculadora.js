fd074e9fa38293c257760d28c127452dad8a2b0f
Jeremy Hernández Balderas <jeremy.hdez9@gmail.com>
2022-09-28 08:04:33 -0500

    inicio calculadora fisica por Jeremy

---
+++ JavaScript/Calculadora.js
@@ -0,0 +1,13 @@
+var velocidadIMRUA;
+var velocidadFMRUA;
+var tiempoMRU;
+var velocidadMRU;
+var altura;
+var distanciaMRU;
+var gravedad = 9.81;
+var aceleracion;
+var angulo;
+
+velocidadMRU=distanciaMRU/tiempoMRU;
+distanciaMRU=velocidadMRU*tiempoMRU;
+tiempoMRU=distanciaMRU*velocidadMRU;
