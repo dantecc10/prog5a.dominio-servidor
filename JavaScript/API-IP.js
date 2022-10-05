397fdb607c03530bfb22a962d86bd9f12d3a37f7
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-10-05 17:58:16 -0500

    previo a IP

--- JavaScript/API-IP.js
+++ JavaScript/API-IP.js
@@ -26,3 +26,20 @@ function investigaIP(urlBase, finURL, dirIP) { //Muestra la informaci√≥n a trav√
         console.log("Error: ", error);
     });
 }
+
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script type="text/javascript">
+    var ip = dirIP;
+    var api_key = "";
+    $(function () {
+       $.ajax({
+           url: "https://geo.ipify.org/api/v1",
+           data: {apiKey: api_key, ipAddress: ip},
+           success: function(data) {
+               $("body").append("<pre>"+ JSON.stringify(data,"",2)+"</pre>");
+           }
+       });
+    });
+</script>
