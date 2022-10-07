910ff82f738c61ecb5ca9148ff2825638bef921a
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-10-07 14:28:37 -0500

    mensaje diferente

--- index2.php
+++ index2.php
@@ -65,8 +65,8 @@
         <div class="container">
             <div class="intro-text">
                 <div class="intro-lead-in"><span><span style="color: rgb(245, 245, 240); background-color: rgba(10, 10, 10, 0.58);">¡Bienvenido al
-                            servidor del PROG5A!</span></span></div>
-                <div class="intro-heading text-uppercase"><span><span style="color: rgb(243, 248, 248); background-color: rgba(6, 6, 6, 0.58);">Explora el contenido.</span></span></div><a class="btn btn-primary btn-xl text-uppercase" role="button" href="#services">Conocer más</a>
+                            servidor del</span></span></div>
+                <div class="intro-heading text-uppercase"><span><span style="color: rgb(243, 248, 248); background-color: rgba(6, 6, 6, 0.58);">PROG5A!</span></span></div><a class="btn btn-primary btn-xl text-uppercase" role="button" href="#services">Explorar</a>
             </div>
         </div>
     </header>
