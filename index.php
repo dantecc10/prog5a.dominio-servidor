580ee89de40e92802a3ee1013e2c2c733ed21eeb
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-10-07 09:19:05 -0500

    cambios y movimientos

--- index.php
+++ index.php
@@ -16,7 +16,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PROG5A - Inicio</title>
-
 </head>
 
 <body onload="getLocation();">
