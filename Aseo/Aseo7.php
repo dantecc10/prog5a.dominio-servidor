6abb0de92793ed6cd19a3a9ab1833dc8439c63ad
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-10-12 10:39:55 -0500

    rutas

--- Aseo/Aseo7.php
+++ Aseo/Aseo7.php
@@ -8,7 +8,7 @@ if (empty($_SESSION['ID'])) {
 <html lang="es">
 
 <head>
-    <link rel="shortcut icon" href="../IMG/Logo-PROG5A.jpg" type="image/x-icon">
+    <link rel="shortcut icon" href="../IMG/Logo-PROG5A.png" type="image/x-icon">
     <link id="EtiquetaCSS" rel="Stylesheet" href="../CSS/CSS GLOBAL - TEMA INSTITUCIONAL PUEBLA.css" Type="text/css" MEDIA="screen">
     <!--<link rel="Stylesheet" href="CSS/Formulario.css" Type="text/css" MEDIA="screen">-->
     <!--<link id="Oculta/Muestra" rel="Stylesheet" href="../CSS/CSS OCULTAMIENTO.css" Type="text/css" MEDIA="screen">-->
@@ -64,7 +64,7 @@ if (empty($_SESSION['ID'])) {
     <header id="Encabezado">
         <div id="Contenedor-Encabezado">
             <div id="Contenido-Encabezado">
-                <h1>PROG5A</h1> <a href="index.php"> <img id="Logo" src="IMG/Logo-PROG5A.jpg" alt="prog5a.castelancarpinteyro.club">
+                <h1>PROG5A</h1> <a href="index.php"> <img id="Logo" src="IMG/Logo-PROG5A.png" alt="prog5a.castelancarpinteyro.club">
                 </a>
                 <div class="Menú-Desplegable"> <button class="Botón-Desplegar">Menú</button>
                     <div class="Contenido-Desplegable">
